4aa4236617006d2756c084ac9a491b8e03d10bb3
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-11-03 12:18:36 +0300

    Create Par_Spot_Curve.py

---
+++ CFA/Par_Spot_Curve.py
@@ -0,0 +1,10 @@
+#https://analystprep.com/study-notes/cfa-level-2/describe-how-zero-coupon-rates-spot-rates-may-be-obtained-from-the-par-curve-by-bootstrapping/
+
+Annual Par-Rates
+Year  Par Rate
+1     2.00
+2     2.60
+3     2.90
+4     3.80
+ 
+\begin{align*} 1 &=\frac{0.026}{1.02}+\frac{(1+0.026)}{\left(1+r\left(2\right)\right)^2} \\ r (2) & =2.61\% \end{align*}
