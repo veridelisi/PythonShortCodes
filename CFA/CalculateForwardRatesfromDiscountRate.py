6371d9f07fad5ce5a5e01cc55a21f91818705b41
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-12-12 15:36:57 +0300

    Create CalculateForwardRatesfromDiscountRate.py

---
+++ CFA/CalculateForwardRatesfromDiscountRate.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Set up the variables
+spot_rates = {
+    1: 0.055,
+    2: 0.06,
+    3: 0.0625,
+    4: 0.065,
+    5: 0.07,
+}
+
+# Calculate discount factors
+discount_factors = [(1 / (1 + spot_rates[i]) ** i) for i in range(1, 6)]
+
+# Calculate forward rates as specified
+forward_rates = [(discount_factors[i] / discount_factors[i + 1]) - 1 for i in range(4)]
+forward_rates = [rate * 100 for rate in forward_rates]  # Convert to percentage
+
+# Create a DataFrame to display the results
+result_table = pd.DataFrame(index=range(1, 6))
+result_table.index.name = 'Year'
+result_table['Spot Rate'] = [spot_rates[i] for i in range(1, 6)]
+result_table['Discount Factor'] = discount_factors
+result_table['Forward Rate'] = [0] + forward_rates  # First year forward rate is assumed to be 0
+
+# Display the results
+print(result_table)
